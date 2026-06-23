7d8c7ae5e21832cce8a93546843aa69e38163e75
nowaljin <nowaljin13@gmail.com>
2026-06-23 10:26:38 +0900

    DeckVisuals

--- Assets/Scripts/Deck.cs
+++ Assets/Scripts/Deck.cs
@@ -2,15 +2,45 @@ using NUnit.Framework;
 using System;
 using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 
 public class Deck : MonoBehaviour
 {
     [SerializeField] private List<CardData> drawPile= new List<CardData>();
 
+    [SerializeField] private GameObject cardBack;
+
+    private const float VERTICAL_SPACING = .1f;
+
     void Start()
     {
-        
+        Debug.Log(DrawCard());
+        DeckDrawVisuals();
+
     }
+
+    public CardData DrawCard()
+    {
+
+        if (drawPile.Count > 0)
+        {
+            int topIndex = drawPile.Count - 1;
+            CardData data = drawPile[topIndex];
+            drawPile.RemoveAt(topIndex);
+            return data;
+        }
+        return null;
+    }
+
+    private void DeckDrawVisuals()
+    {
+        for (int i = 0; i < drawPile.Count; i++)
+        {
+            GameObject newCardBack = Instantiate(cardBack, transform);
+            newCardBack.transform.localPosition = new Vector3(0f, -i * VERTICAL_SPACING,0f);
+        }
+    }
+
 
   
   
