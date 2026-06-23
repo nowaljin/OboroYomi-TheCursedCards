c95f7c792a723633a6b48dd9179cc695de42095e
nowaljin <nowaljin13@gmail.com>
2026-06-23 09:51:08 +0900

    updateDecks

---
+++ Assets/Scripts/Deck.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Deck : MonoBehaviour
+{
+    [SerializeField] private List<CardData> drawPile= new List<CardData>();
+
+    void Start()
+    {
+        
+    }
+
+  
+  
+}
