7285f0c8e5b71f81fab1764454fdac2cdb065130
nowaljin <nowaljin13@gmail.com>
2026-06-25 16:39:04 +0900

    update

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,6 @@
-using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class Card : MonoBehaviour
@@ -23,6 +23,8 @@ public class Card : MonoBehaviour
     private SortingGroup sortingGroup;
 
     private int originalSortingOrder;
+
+    private static bool isBeingDragged = false;
 
    
 
@@ -53,6 +55,10 @@ public class Card : MonoBehaviour
 
     private void OnMouseEnter()
     {
+         if (isBeingDragged)
+        {
+            return;
+        }
         Debug.Log("Mouse Entered");
         transform.localScale = orginalScale * hoverScale;
         transform.localPosition += new Vector3(0, hoverOffset, 0f);
@@ -61,9 +67,37 @@ public class Card : MonoBehaviour
 
     private void OnMouseExit()
     {
+        if (isBeingDragged)
+        {
+            return;
+        }
         Debug.Log("Mouse Exit");
         transform.localScale = orginalScale;
         transform.localPosition = orginalPosition;
         sortingGroup.sortingOrder = originalSortingOrder;
     }
+
+    private void OnMouseDrag()
+    {
+        isBeingDragged = true;
+        gameObject.transform.position = GetMousePosition();
+    }
+
+    private Vector3 GetMousePosition()
+
+    {
+        Vector3 mousePosition = Mouse.current.position.ReadValue();
+        mousePosition.z = transform.position.z - Camera.main.transform.position.z;
+        return Camera.main.ScreenToWorldPoint(mousePosition);
+    }
+
+    private void OnMUp()
+    {
+        Debug.Log("Mouse up");
+        isBeingDragged = false;
+        transform.localScale = orginalScale;
+        transform.localPosition = orginalPosition;
+        sortingGroup.sortingOrder = originalSortingOrder;
+    }
+
 }
